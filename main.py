c852bac68f5cf879165dfd4f7e7eca988b394d3a
tima <timur.dootaliev.td@gmail.com>
2026-07-01 20:23:15 +0600

    merge command added

--- main.py
+++ main.py
@@ -23,3 +23,7 @@
 # git branch - вывод списка веток
 # git branch <new_branch_name> - создание новой ветки
 # git checkout <branch_name> - переход на указанную ветку
+
+# git checkout -b <new_branch_name> - создание новой ветки и переход на указанную ветку
+
+# git merge <branch_name> - склеивание веток. Склеивает указанную ветку к текущей
